aad3c61360cb4a1f3b2d0859224f61c151bb048b
David Y. Stephenson <david@davidystephenson.com>
2020-02-14 13:48:42 +0100

    Receive and parse events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 class App extends React.Component {
+  stream = new EventSource('http://localhost:4000/stream')
+
+  componentDidMount () {
+    this.stream.onmessage = function (
+      event
+    ) {
+      console.log('event test:', event)
+
+      const { data } = event
+      console.log('data test:', data)
+
+      const parsed = JSON.parse(data)
+      console.log('parsed test:', parsed)
+    }
+  }
+
   render () {
     return <div>hello</div>
   }

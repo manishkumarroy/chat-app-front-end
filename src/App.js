ad7cd3cef2410641ee56745d8c8d53b6e3b0aa6f
David Y. Stephenson <david@davidystephenson.com>
2020-02-14 15:18:55 +0100

    Fully controlled form

--- src/App.js
+++ src/App.js
@@ -2,28 +2,26 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-    messages: []
+    messages: [],
+    value: ''
   }
 
   stream = new EventSource('http://localhost:4000/stream')
 
   componentDidMount () {
     this.stream.onmessage = event => {
-      console.log('event test:', event)
-
       const { data } = event
-      console.log('data test:', data)
 
       const action = JSON.parse(data)
       console.log('action test:', action)
 
       const { type, payload } = action
 
-      if (type === 'MESSAGES') {
+      if (type === 'ALL_MESSAGES') {
         this.setState({ messages: payload })
       }
 
-      if (type === 'MESSAGE') {
+      if (type === 'ONE_MESSAGE') {
         const messages = [
           ...this.state.messages,
           payload
@@ -34,7 +32,25 @@ class App extends React.Component {
     }
   }
 
+  onSubmit = (event) => {
+    event.preventDefault()
+
+    console.log('value test:', this.state.value)
+  }
+
+  onChange = (event) => {
+    const { value } = event.target
+
+    this.setState({ value })
+  }
+
+  clear = () => {
+    console.log('clear test')
+    this.setState({ value: '' })
+  }
+
   render () {
+    console.log('render state test:', this.state)
     const paragraphs = this
       .state
       .messages
@@ -44,7 +60,23 @@ class App extends React.Component {
         {message.text}
       </p>)
 
-    return <div>{paragraphs}</div>
+    return <div>
+      <form onSubmit={this.onSubmit}>
+        <input
+          onChange={this.onChange}
+          type='text'
+          value={this.state.value}
+        />
+
+        <button>submit</button>
+      </form>
+
+      <button onClick={this.clear}>
+        Clear
+      </button>
+
+      {paragraphs}
+    </div>
   }
 }
 
